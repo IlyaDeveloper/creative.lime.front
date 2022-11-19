a10c8b618a510751027506573e93a50293ce3fc1
Илья Пащук <illyadeveloper@gmail.com>
2022-11-19 17:12:46 +0200

    Add js and fix container

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const resetScroll = () => {
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+    clearTimeout();
+  }, 0);
+}
+
+const customScroll = () => {
+  let win = document.querySelector('html').classList,
+    checkPlatform = window.navigator.platform.includes('Win');
+
+  checkPlatform
+    ? win.add('--custom-scroll')
+    : win.remove('--custom-scroll');
+}
+
+const app = () => {
+  resetScroll();
+  customScroll();
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.group('Initialization page')
+  app()
+  console.groupEnd()
+  console.log('✅ Finally Loaded Page')
+});
